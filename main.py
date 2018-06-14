a94e245ea54e1d00c4e5b15d7a8d71dbb77b2ef5
Mashiat Sarker Shakkhar <mashiat.sarker@gmail.com>
2018-06-14 19:21:50 -0400

    A sort of game loop.

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-import cmd
-
-
-class Shell(cmd.Cmd):
-    pass
-
 class Game:
     WINNING_COMBINATIONS = [(6, 7, 8),
                             (3, 4, 5),
@@ -14,7 +8,7 @@ class Game:
                             (2, 5, 8),
                             (0, 1, 2)]
     def __init__(self):
-        self._board = [''] * 9
+        self._board = [' '] * 9
 
     def play_move(self, player, move):
         """Returns a empty slot that player can take"""
@@ -23,8 +17,9 @@ class Game:
     def print_board(self):
         """prints the board in a nice matrix"""
         for i in range(3):
-            print '-------'
-            print '| | | |'
+            print('-------')
+            print('|' + '|'.join(self._board[i * 3 : i * 3 + 3]) + '|')
+        print('-------')
 
 
     def find_move_for_player(self, player):
@@ -32,9 +27,26 @@ class Game:
         pass
 
 
+    def parse_move(self):
+        """
+        '[abc][012] first column, second row'
+        """
+        move = input()
+        if not move:
+            raise SystemExit
+        print(move)
+
     def check(self):
         """Check if the game is over or not"""
-        pass
+        return True
 
 
-
+if __name__ == '__main__':
+    print("Let's Play!")
+    print("You're Xs, I'm Os!")
+    game = Game()
+    while game.check():
+        print('Your move: ', end='')
+        move = game.parse_move()
+        game.play_move('x', move)
+        game.print_board()
