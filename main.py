e00f9a12f95d50ede4289dc1eb7dc64a677a4c0c
Mashiat Sarker Shakkhar <mashiat.sarker@gmail.com>
2018-06-14 19:07:57 -0400

    More scaffolding.

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import cmd
+
+
+class Shell(cmd.Cmd):
+    pass
+
 class Game:
     WINNING_COMBINATIONS = [(6, 7, 8),
                             (3, 4, 5),
@@ -10,21 +16,25 @@ class Game:
     def __init__(self):
         self._board = [''] * 9
 
-    _board: 9 element list
-
     def play_move(self, player, move):
         """Returns a empty slot that player can take"""
+        pass
 
     def print_board(self):
         """prints the board in a nice matrix"""
+        for i in range(3):
+            print '-------'
+            print '| | | |'
 
 
     def find_move_for_player(self, player):
         """return a position for the player"""
+        pass
 
 
     def check(self):
         """Check if the game is over or not"""
+        pass
 
 
 
